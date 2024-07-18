fa443f5e0d2d188e4d846b6455e2477bf56b2a86
Omkar Dalavi <133959956+omkardalavipatil@users.noreply.github.com>
2024-07-19 00:26:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-// Sticky Navbar
-window.addEventListener("scroll", function () {
-    let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 100);
-  });
-  
-  // Show scrool btn when scroll down
+document.addEventListener("DOMContentLoaded", function() {
+  // Sticky Navbar
   window.addEventListener("scroll", function () {
-    let scrool = document.querySelector("#scrool-up");
-    scrool.classList.toggle("srcl", window.scrollY > 600);
+      let header = document.querySelector("header");
+      header.classList.toggle("sticky", window.scrollY > 100);
   });
+
+  // Show scroll btn when scroll down
+  window.addEventListener("scroll", function () {
+      let scroll = document.querySelector("#scrool-up");
+      scroll.classList.toggle("srcl", window.scrollY > 600);
+  });
+
+  // Projects button click event
+  document.querySelector("#projects-btn").addEventListener("click", function(event) {
+      event.preventDefault(); // Prevent the default anchor behavior
+      document.querySelector("#projects").scrollIntoView({ behavior: 'smooth' });
+  });
+});
